74d862c61c17f1a41b591227472cea39c46b71ce
danst0 <daniel@dumke.me>
2016-04-26 19:41:32 +0200

    Update pcb2gcode.rb
    https://github.com/pcb2gcode/pcb2gcode/issues/33#issuecomment-214496534

--- pcb2gcode.rb
+++ pcb2gcode.rb
@@ -1,46 +1,116 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Pcb2gcode < Formula
   desc "Command-line tool for isolation routing and drilling of PCBs"
-  homepage "https://github.com/pcb2gcode/pcb2gcode#commonbuild"
-  url "https://github.com/pcb2gcode/pcb2gcode.git"
-  #url "https://github.com/pcb2gcode/pcb2gcode/releases/download/v1.2.2/pcb2gcode-1.2.2.tar.gz"
-  version "1.2.2"
-  sha256 "8886a068a1726b5d1787e3fd5891f76a37415429081441bd82cca2a256082924"
+  homepage "https://github.com/pcb2gcode/pcb2gcode"
+  url "https://github.com/pcb2gcode/pcb2gcode/releases/download/v1.3.0/pcb2gcode-1.3.0.tar.gz"
+  sha256 "07edd07dcbfc2054c2dbccfc328e091749b08a23431e586e21db8100a2f63749"
 
-  # depends_on "cmake" => :build
-  #depends_on :x11 # if your formula requires any X11/XQuartz components
+  head do
+    url "https://github.com/pcb2gcode/pcb2gcode.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   depends_on "boost"
-  depends_on "pkg-config"
-  #depends_on "glibmm"
   depends_on "gtkmm"
   depends_on "gerbv"
-  depends_on "autoconf"
-  depends_on "automake"
-  depends_on "libtool"
+
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    #system "/usr/local/bin/glibtoolize"
-    #system "/usr/local/bin/aclocal"
-    #system "/usr/local/bin/automake", "--add-missing"
-
-
-    system "/usr/local/bin/autoreconf -i"
+    system "autoreconf", "-i" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    #ENV['CFLAGS']="-std=c++11"
-    
-    system "make", "-j"
-    system "make", "install" # if this fails, try separate make/make install steps
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make", "install"
   end
 
   test do
-    system "pcb2gcode --version"
+    (testpath/"front.gbr").write <<-EOS.undent
+      %FSLAX46Y46*%
+      %MOMM*%
+      G01*
+      %ADD10C,0.100000*%
+      %ADD11R,2.032000X2.032000*%
+      %ADD12O,2.032000X2.032000*%
+      %ADD13C,0.250000*%
+      D10*
+      D11*
+      X127000000Y-63500000D03*
+      D12*
+      X127000000Y-66040000D03*
+      D13*
+      X124460000Y-66040000D02*
+      X127000000Y-66040000D01*
+      X124460000Y-63500000D02*
+      X124460000Y-66040000D01*
+      X127000000Y-63500000D02*
+      X124460000Y-63500000D01*
+      M02*
+    EOS
+    (testpath/"edge.gbr").write <<-EOS.undent
+      %FSLAX46Y46*%
+      %MOMM*%
+      G01*
+      %ADD10C,0.100000*%
+      %ADD11C,0.150000*%
+      D10*
+      D11*
+      X123190000Y-67310000D02*
+      X123190000Y-62230000D01*
+      X128270000Y-67310000D02*
+      X123190000Y-67310000D01*
+      X128270000Y-62230000D02*
+      X128270000Y-67310000D01*
+      X123190000Y-62230000D02*
+      X128270000Y-62230000D01*
+      M02*
+    EOS
+    (testpath/"drill.drl").write <<-EOS.undent
+      M48
+      FMAT,2
+      METRIC,TZ
+      T1C1.016
+      %
+      G90
+      G05
+      M71
+      T1
+      X127.Y-63.5
+      X127.Y-66.04
+      T0
+      M30
+    EOS
+    (testpath/"millproject").write <<-EOS.undent
+      dpi=500
+      metric=true
+      zchange=10
+      zsafe=5
+      mill-feed=600
+      mill-speed=10000
+      offset=0.1
+      zwork=-0.05
+      drill-feed=1000
+      drill-speed=10000
+      zdrill=-2.5
+      bridges=0.5
+      bridgesnum=4
+      cut-feed=600
+      cut-infeed=10
+      cut-speed=10000
+      cutter-diameter=3
+      fill-outline=true
+      outline-width=0.15
+      zbridges=-0.6
+      zcut=-2.5
+      al-front=true
+      al-probefeed=100
+      al-x=15
+      al-y=15
+      software=LinuxCNC
+    EOS
+    system "#{bin}/pcb2gcode", "--front=front.gbr",
+                               "--outline=edge.gbr",
+                               "--drill=drill.drl"
   end
 end
